2a87941d16df7969c0d4db7594e17174d2130f22
Eduardo Santos Delgado <eduardosa@microsoft.com>
2023-09-04 07:48:01 -0700

    Abstract Factory Pattern - Rust

---
+++ Rust/creational/abstract-factory/macos-gui/factory.rs
@@ -0,0 +1,18 @@
+use gui::{Button, Checkbox, GuiFactory};
+
+use create::{button::MacButton, checkbox::MacCheckbox};
+
+pub struct MacFactory;
+
+impl GuiFactory for MacFactory {
+    type B: MacButton;
+    type C :: MacCheckbox
+
+    fn create_button(&self) -> MacButton {
+        MacButton
+    }
+
+    fn create_checkbox(&self) -> MacCheckbox {
+        MacCheckbox
+    }
+}
---
+++ Rust/creational/abstract-factory/windows-gui/factory.rs
@@ -0,0 +1,18 @@
+use gui::{Button, Checkbox, GuiFactory};
+
+use create::{button::WindowsButton, checkbox::WindowsCheckbox};
+
+pub struct WindowsFactory;
+
+impl GuiFactory for WindowsFactory {
+    type B: WindowsButton;
+    type C :: WindowsCheckbox
+
+    fn create_button(&self) -> WindowsButton {
+        WindowsButton
+    }
+
+    fn create_checkbox(&self) -> WindowsCheckbox {
+        WindowsCheckbox
+    }
+}
